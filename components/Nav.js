274099522f4f6726a7040666313f0d33355124a7
Jesus Sanchez <jsanchez@Jesuss-MacBook-Pro.local>
2022-11-08 12:35:54 -0700

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react"
+
+
+export default function Nav(){
+    return (
+        <nav className="navbar">
+            <h3><a href="index.html">Home</a></h3>
+            <h3><a href="index.html">Contact</a></h3>
+            <h3><a href="index.html">Gallery</a></h3>
+        </nav>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Nav from "./components/Nav"
+
+export default function App(){
+    return (
+        <div>
+            <Nav />
+            <h1>hello</h1>
+        </div>
+    )
+}
